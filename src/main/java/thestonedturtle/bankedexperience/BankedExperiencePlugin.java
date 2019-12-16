74eb9b7e97b1ffa8f97ad34e477acfaf3ad7fde0
TheStonedTurtle <mgoodwin559@gmail.com>
2019-12-16 08:58:27 -0800

    Add banked exp plugin framework and add to sidebar

--- src/main/java/thestonedturtle/bankedexperience/BankedExperiencePlugin.java
+++ src/main/java/thestonedturtle/bankedexperience/BankedExperiencePlugin.java
@@ -1,16 +1,19 @@
 package thestonedturtle.bankedexperience;
 
 import com.google.inject.Provides;
+import java.awt.image.BufferedImage;
 import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
-import net.runelite.api.GameState;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.events.GameStateChanged;
+import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
-import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.game.ItemManager;
+import net.runelite.client.game.SkillIconManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
-import lombok.extern.slf4j.Slf4j;
+import net.runelite.client.ui.ClientToolbar;
+import net.runelite.client.ui.NavigationButton;
+import net.runelite.client.util.ImageUtil;
 
 @Slf4j
 @PluginDescriptor(
@@ -18,36 +21,49 @@ import lombok.extern.slf4j.Slf4j;
 )
 public class BankedExperiencePlugin extends Plugin
 {
+	private final static BufferedImage ICON = ImageUtil.getResourceStreamFromClass(BankedExperiencePlugin.class, "banked.png");
+
 	@Inject
 	private Client client;
 
 	@Inject
+	private ClientToolbar clientToolbar;
+
+	@Inject
+	private ItemManager itemManager;
+
+	@Inject
+	private SkillIconManager skillIconManager;
+
+	@Inject
 	private BankedExperienceConfig config;
-
-	@Override
-	protected void startUp() throws Exception
-	{
-		log.info("Banked Experience started!");
-	}
-
-	@Override
-	protected void shutDown() throws Exception
-	{
-		log.info("Banked Experience stopped!");
-	}
-
-	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
-	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Banked Experience says " + config.greeting(), null);
-		}
-	}
 
 	@Provides
 	BankedExperienceConfig provideConfig(ConfigManager configManager)
 	{
 		return configManager.getConfig(BankedExperienceConfig.class);
+	}
+
+	private NavigationButton navButton;
+	private boolean prepared = false;
+
+	@Override
+	protected void startUp() throws Exception
+	{
+		final BankedCalculatorPanel panel = new BankedCalculatorPanel(client, config, skillIconManager, itemManager);
+		navButton = NavigationButton.builder()
+			.tooltip("Banked XP")
+			.icon(ICON)
+			.priority(6)
+			.panel(panel)
+			.build();
+
+		clientToolbar.addNavigation(navButton);
+	}
+
+	@Override
+	protected void shutDown() throws Exception
+	{
+		clientToolbar.removeNavigation(navButton);
 	}
 }
