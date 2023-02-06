4d760789a04d91026f3c32bbee1255003e091a10
aladdin <seaifan.aladdin@gmail.com>
2023-02-05 19:24:23 -0500

    Renamed variables to make code clearer

--- src/main/java/thestonedturtle/bankedexperience/data/modifiers/CarpentersOutfit.java
+++ src/main/java/thestonedturtle/bankedexperience/data/modifiers/CarpentersOutfit.java
@@ -1,26 +1,19 @@
 package thestonedturtle.bankedexperience.data.modifiers;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import lombok.AllArgsConstructor;
 import net.runelite.api.ItemComposition;
 import net.runelite.api.Skill;
 import net.runelite.client.game.ItemManager;
 import thestonedturtle.bankedexperience.data.Activity;
 
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Map;
 
 public class CarpentersOutfit extends SkillingOutfit {
 
-	/**
-	 * parameters calculated from here
-	 * https://docs.google.com/spreadsheets/d/1573_E5suKS7OY2E3utGpBkuaReiF86hTh0p8PLqi2II/edit?usp=sharing
-	 */
-	public final static double[] MAHOGANY_HOMES_PLANK_PARAMETERS = new double[]{39.95d, 53.69d};
-	public final static double[] MAHOGANY_HOMES_OAK_PARAMETERS = new double[]{66.16d, 133.84d};
-	public final static double[] MAHOGANY_HOMES_TEAK_PARAMETERS = new double[]{87.73d, 200.12d};
-	public final static double[] MAHOGANY_HOMES_MAHOGANY_PARAMETERS = new double[]{127.26d, 218.86d};
-	private final Map<Activity, double[]> mahoganyHomesActivityToParametersMap;
+	private final Map<Activity, XpPerTask> xpPerMahoganyHomesActivityMap;
 
 
 	private static final Collection<Activity> EXCLUDED = ImmutableSet.of(
@@ -31,26 +24,32 @@ public class CarpentersOutfit extends SkillingOutfit {
 		super(Skill.CONSTRUCTION, "Carpenter's Outfit",
 				null, EXCLUDED, itemManager, items);
 
-		mahoganyHomesActivityToParametersMap = new HashMap<>(4);
-		mahoganyHomesActivityToParametersMap.put(Activity.MAHOGANY_HOMES_PLANK, MAHOGANY_HOMES_PLANK_PARAMETERS);
-		mahoganyHomesActivityToParametersMap.put(Activity.MAHOGANY_HOMES_OAK, MAHOGANY_HOMES_OAK_PARAMETERS);
-		mahoganyHomesActivityToParametersMap.put(Activity.MAHOGANY_HOMES_TEAK, MAHOGANY_HOMES_TEAK_PARAMETERS);
-		mahoganyHomesActivityToParametersMap.put(Activity.MAHOGANY_HOMES_MAHOGANY, MAHOGANY_HOMES_MAHOGANY_PARAMETERS);
+		xpPerMahoganyHomesActivityMap = ImmutableMap.<Activity, XpPerTask>builder()
+				.put(Activity.MAHOGANY_HOMES_PLANK, new XpPerTask(39.95f, 53.69f))
+				.put(Activity.MAHOGANY_HOMES_OAK, new XpPerTask(66.16f, 133.84f))
+				.put(Activity.MAHOGANY_HOMES_TEAK, new XpPerTask(87.73f, 200.12f))
+				.put(Activity.MAHOGANY_HOMES_MAHOGANY, new XpPerTask(127.26f, 218.86f))
+				.build();
 	}
 
 	@Override
 	public double appliedXpRate(Activity activity) {
-		double[] parameters = mahoganyHomesActivityToParametersMap.get(activity);
-		if (parameters != null && parameters.length == 2) {
+		XpPerTask xpPerTask = xpPerMahoganyHomesActivityMap.get(activity);
+		if (xpPerTask != null) {
 			double bonusXP = calculateBonusXPMultiplier();
-			//m is the average xp per plank excluding completion xp
-			//b is the average completion xp per plank
-			double m = parameters[0];
-			double b = parameters[1];
 
-			return m * bonusXP + b;
+			double averagePlankXp = xpPerTask.averagePlankXp;
+			double averageCompletionXpPerPlank = xpPerTask.averageCompletionXpPerPlank;
+
+			return averagePlankXp * bonusXP + averageCompletionXpPerPlank;
 		} else {
 			return super.appliedXpRate(activity);
 		}
+	}
+
+	@AllArgsConstructor
+	private static class XpPerTask {
+		private float averagePlankXp;
+		private float averageCompletionXpPerPlank;
 	}
 }
