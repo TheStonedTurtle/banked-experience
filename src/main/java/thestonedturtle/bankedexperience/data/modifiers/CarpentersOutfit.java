565f81985d01473058e9bbaf21e001bea26b0a38
TheStonedTurtle <mgoodwin559@gmail.com>
2023-02-11 15:50:27 -0800

    Revert "Added checks to carpenters outfit xp rates for mahogany homes that logs a warning if base xp rate does not match activity xp rate."
    This reverts commit 6b49412ecfdff2ac4ed5754d06d68d583e1ec726.

--- src/main/java/thestonedturtle/bankedexperience/data/modifiers/CarpentersOutfit.java
+++ src/main/java/thestonedturtle/bankedexperience/data/modifiers/CarpentersOutfit.java
@@ -3,7 +3,6 @@ package thestonedturtle.bankedexperience.data.modifiers;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ItemComposition;
 import net.runelite.api.Skill;
 import net.runelite.client.game.ItemManager;
@@ -12,7 +11,6 @@ import thestonedturtle.bankedexperience.data.Activity;
 import java.util.Collection;
 import java.util.Map;
 
-@Slf4j
 public class CarpentersOutfit extends SkillingOutfit
 {
 	private final Map<Activity, XpPerTask> xpPerMahoganyHomesActivityMap;
@@ -32,19 +30,6 @@ public class CarpentersOutfit extends SkillingOutfit
 				.put(Activity.MAHOGANY_HOMES_TEAK, new XpPerTask(87.735f, 200.117f))
 				.put(Activity.MAHOGANY_HOMES_MAHOGANY, new XpPerTask(127.259f, 218.861f))
 				.build();
-
-		final float PRECISION_LEVEL = 0.0001f;
-		for (Activity activity : xpPerMahoganyHomesActivityMap.keySet())
-		{
-			XpPerTask xpPerTask = xpPerMahoganyHomesActivityMap.get(activity);
-
-			float baseXp = xpPerTask.averagePlankXp + xpPerTask.averageCompletionXpPerPlank;
-			boolean isBaseXpSame = Math.abs(baseXp - activity.getXp()) < PRECISION_LEVEL;
-			if (!isBaseXpSame)
-			{
-				log.warn(String.format("xp of Activity %s is %.3f but calculated base xp of carpenters Outfit is calculated to be %.3f", activity.getName(), activity.getXp(), baseXp));
-			}
-		}
 	}
 
 	@Override
